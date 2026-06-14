1ff25afb9113037f6f1f6ac3e91c7113e60ccac3
Shivam Singh <shivamsingh080506@gmail.com>
2026-06-14 11:16:46 +0530

    Add: matchsticks-to-square - submission-1

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        
+        total = sum(matchsticks)
+
+        if total % 4 != 0:
+            return False
+        
+        oneside = total / 4
+        matchsticks.sort(reverse=True)
+        
+        def dfs(i, sides):
+            if i == len(matchsticks):
+                return True
+            for j in range(4):
+                if matchsticks[i] + sides[j] > oneside:
+                    continue
+                
+                sides[j] += matchsticks[i]
+                if dfs(i+1, sides):
+                    return True
+                sides[j] -= matchsticks[i]
+                
+            return False
+        
+
+    
+        return dfs(0, [0] * 4)
+
